daa92f3f5ae346730852f35a6a7a8446829b525b
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-11-24 13:52:37 +0100

    ghfd

--- exercice 1/exercice1.js
+++ exercice 1/exercice1.js
@@ -1,6 +1,13 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+let x = 2;
+let y = 5;
 
-if x > y
+
+if (x > y) {
     document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
+}
 
+else {
+    document.getElementById('monDiv').innerHTML = "La variable x est inferrieure à la variable y";
+}
 
--- exercice3/exercice3.js
+++ exercice3/exercice3.js
@@ -1,10 +1,13 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+let x = 5;
+let y = 5;
 
-if()
-{
+x += 3;
 
+if(x === y) {
+    document.getElementById('monDiv').innerHTML = "La variable x est strictement égal à la variable y";
 }
 
-{
-
+else {
+    document.getElementById('monDiv').innerHTML = "La variable x n'est pas strictement égal à la variable y";
 }
